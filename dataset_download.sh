89d4cf85f4fcc261c63ff9e701bc99efac110ef8
HanYunhui <69156164+Timer-x@users.noreply.github.com>
2022-08-01 22:51:32 +0800

    Update dataset_download.sh

--- dataset_download.sh
+++ dataset_download.sh
@@ -34,16 +34,3 @@ wget https://data.megengine.org.cn/research/realflow/RFAB-flow-Apart3.zip
 wget https://data.megengine.org.cn/research/realflow/RFAB-flow-Bpart0.zip
 wget https://data.megengine.org.cn/research/realflow/RFAB-flow-Bpart1.zip
 cd ..
-
-
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/models.zip s3://engine-data/research/realflow/models.zip
-
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/RF-Davis/RF-Davis-flow.zip s3://engine-data/research/realflow/RF-Davis-flow.zip
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/ALOV/part0/RFAB-flow-Apart0.zip s3://engine-data/research/realflow/RFAB-flow-Apart0.zip
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/ALOV/part1/RFAB-flow-Apart1.zip s3://engine-data/research/realflow/RFAB-flow-Apart1.zip
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/ALOV/part2/RFAB-flow-Apart2.zip s3://engine-data/research/realflow/RFAB-flow-Apart2.zip
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/ALOV/part3/RFAB-flow-Apart3.zip s3://engine-data/research/realflow/RFAB-flow-Apart3.zip
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/BDD100K/part0/RFAB-flow-Bpart0.zip s3://engine-data/research/realflow/RFAB-flow-Bpart0.zip
-#aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/BDD100K/part1/RFAB-flow-Bpart1.zip s3://engine-data/research/realflow/RFAB-flow-Bpart1.zip
-aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/BDD100K/part2/RFAB-flow-Bpart2.zip s3://engine-data/research/realflow/RFAB-flow-Bpart2.zip
-aws s3 --endpoint-url=http://oss-cn-beijing.aliyuncs.com --profile=aliyun cp /data/ECCV/BDD100K/part3/RFAB-flow-Bpart3.zip s3://engine-data/research/realflow/RFAB-flow-Bpart3.zip

6cdf0a6d76592236307eacf8011193446d80856c
HanYunhui <69156164+Timer-x@users.noreply.github.com>
2022-08-02 17:24:59 +0800

    Update dataset_download.sh

--- dataset_download.sh
+++ dataset_download.sh
@@ -6,23 +6,27 @@ cd RF_dataset
 mkdir RF-Ktrain
 cd RF-Ktrain
 wget https://data.megengine.org.cn/research/realflow/RF-Ktrain-flow.zip
+wget https://data.megengine.org.cn/research/realflow/RF-Ktrain-img.zip
 cd ..
 
 # RF-KTest
 mkdir RF-KTest
 cd RF-KTest
 wget https://data.megengine.org.cn/research/realflow/RF-KTest-flow.zip
+wget https://data.megengine.org.cn/research/realflow/RF-KTest-img.zip
 cd ..
 
 # RF-Sintel
 mkdir RF-Sintel
 wget https://data.megengine.org.cn/research/realflow/RFAB-sintel-flow.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-sintel-img.zip
 cd RF-Sintel
 
 # RF-DAVIS
 mkdir RF-DAVIS
 cd RF-DAVIS
 wget https://data.megengine.org.cn/research/realflow/RF-Davis-flow.zip
+wget https://data.megengine.org.cn/research/realflow/RF-Davis-img.zip
 cd ..
 
 # RF-AB
@@ -36,4 +40,12 @@ wget https://data.megengine.org.cn/research/realflow/RFAB-flow-Bpart0.zip
 wget https://data.megengine.org.cn/research/realflow/RFAB-flow-Bpart1.zip
 wget https://data.megengine.org.cn/research/realflow/RFAB-flow-Bpart2.zip
 wget https://data.megengine.org.cn/research/realflow/RFAB-flow-Bpart3.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Apart0.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Apart1.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Apart2.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Apart3.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Bpart0.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Bpart1.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Bpart2.zip
+wget https://data.megengine.org.cn/research/realflow/RFAB-img-Bpart3.zip
 cd ..
